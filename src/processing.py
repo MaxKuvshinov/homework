f7b93adde4fc84f31a8369ea09600014b1708f60
Max Kuvshinov <maksim.kuvshinov94@gmail.com>
2024-05-12 12:48:20 +0300

    modified file main.py and processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,7 @@
-def get_dicts_by_state(data, state='EXECUTED'):
+from typing import List
+
+
+def get_dicts_by_state(data: List[dict], state: str = "EXECUTED") -> List[dict]:
     """Функция, которая возвращает новый словарь"""
     filter_dict = []
     for item in data:
@@ -7,5 +10,6 @@ def get_dicts_by_state(data, state='EXECUTED'):
     return filter_dict
 
 
-def sort_by_data(data, reverse=True):
+def sort_by_date(data: List[dict], reverse: bool = True) -> List[dict]:
+    """Функция, которая сортирует список словарей по ключу 'date'."""
     return sorted(data, key=lambda x: x["date"], reverse=reverse)
