a8ef84d31d00db95d8c56d2150a1a114a54d33a0
Max Kuvshinov <maksim.kuvshinov94@gmail.com>
2024-05-18 19:32:46 +0300

    fixes widget

--- src/widget.py
+++ src/widget.py
@@ -3,18 +3,24 @@ from src.masks import mask_account_number, mask_number_card
 
 def get_mask_account_card(card_or_account: str) -> str:
     """Функция, которая принимает номер карты или счета и маскирует"""
+    result = ""
     words = card_or_account.split()
     number = words[-1]
     description = " ".join(words[:-1])
 
-    if number.isdigit() and len(number) in [16]:
-        masked_number = mask_number_card(number)
-    elif number.isdigit() and len(number) >= 4:
-        masked_number = mask_account_number(number)
+    if number.isdigit():
+        if 13 <= len(number) <= 19:
+            masked_number = mask_number_card(number)
+        elif len(number) == 20:
+            masked_number = mask_account_number(number)
+        else:
+            return card_or_account
     else:
         return card_or_account
 
-    return f"{description} {masked_number}"
+    result = f"{description} {masked_number}"
+
+    return result
 
 
 def get_change_date(date: str) -> str:
