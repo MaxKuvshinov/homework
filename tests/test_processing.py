c2b55eff5fcdd21dda7ddf00c8d00f2e7d35430b
Max Kuvshinov <maksim.kuvshinov94@gmail.com>
2024-05-18 20:45:57 +0300

    modified file test_processing.py

---
+++ tests/test_processing.py
@@ -0,0 +1,115 @@
+import pytest
+
+from src.processing import get_dicts_by_state, sort_by_date
+
+
+def test_get_dicts_by_state(data):
+    assert get_dicts_by_state(data) == [
+        {"date": "2019-07-03T18:35:29.512364", "id": 41428829, "state": "EXECUTED"},
+        {"date": "2018-06-30T02:08:58.425572", "id": 939719570, "state": "EXECUTED"},
+    ]
+
+
+def test_get_dicts_by_none():
+    assert get_dicts_by_state([]) == []
+
+@pytest.mark.parametrize("data, reverse, sorted_list",
+    [
+        (
+            [
+                {
+                    "id": 41428829,
+                    "state": "EXECUTED",
+                    "date": "2019-07-03T18:35:29.512364",
+                },
+                {
+                    "id": 939719570,
+                    "state": "EXECUTED",
+                    "date": "2018-06-30T02:08:58.425572",
+                },
+                {
+                    "id": 594226727,
+                    "state": "CANCELED",
+                    "date": "2018-09-12T21:27:25.241689",
+                },
+                {
+                    "id": 615064591,
+                    "state": "CANCELED",
+                    "date": "2018-10-14T08:21:33.419441",
+                },
+            ],True,
+    [
+        {
+            "id": 41428829,
+            "state": "EXECUTED",
+            "date": "2019-07-03T18:35:29.512364",
+        },
+        {
+            "id": 615064591,
+            "state": "CANCELED",
+            "date": "2018-10-14T08:21:33.419441",
+        },
+        {
+            "id": 594226727,
+            "state": "CANCELED",
+            "date": "2018-09-12T21:27:25.241689",
+        },
+        {
+            "id": 939719570,
+            "state": "EXECUTED",
+            "date": "2018-06-30T02:08:58.425572",
+        },
+    ],
+),(
+    [
+        {
+            "id": 41428829,
+            "state": "EXECUTED",
+            "date": "2019-07-03T18:35:29.512364",
+        },
+        {
+            "id": 939719570,
+            "state": "EXECUTED",
+            "date": "2018-06-30T02:08:58.425572",
+        },
+        {
+            "id": 594226727,
+            "state": "CANCELED",
+            "date": "2018-09-12T21:27:25.241689",
+        },
+        {
+            "id": 615064591,
+            "state": "CANCELED",
+            "date": "2018-10-14T08:21:33.419441",
+        },
+    ],False,
+            [
+                {
+                    "id": 939719570,
+                    "state": "EXECUTED",
+                    "date": "2018-06-30T02:08:58.425572",
+                },
+                {
+                    "id": 594226727,
+                    "state": "CANCELED",
+                    "date": "2018-09-12T21:27:25.241689",
+                },
+                {
+                    "id": 615064591,
+                    "state": "CANCELED",
+                    "date": "2018-10-14T08:21:33.419441",
+                },
+                {
+                    "id": 41428829,
+                    "state": "EXECUTED",
+                    "date": "2019-07-03T18:35:29.512364",
+                },
+            ],
+        ),
+    ],
+)
+
+def test_sort_by_date(data, reverse, sorted_list):
+    assert sort_by_date(data, reverse) == sorted_list
+
+
