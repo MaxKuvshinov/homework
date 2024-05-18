bcb913bbdfa0a75f088100dea14e97f146f8de81
Max Kuvshinov <maksim.kuvshinov94@gmail.com>
2024-05-18 20:10:28 +0300

    modified file test_widget.py

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -2,8 +2,7 @@ import pytest
 
 from src.masks import mask_number_card, mask_account_number
 
-@pytest.mark.parametrize(
-    "number_card, masked_number",
+@pytest.mark.parametrize("number_card, masked_number",
     [
         ("7000792289606361", "7000 79** **** 6361"),
         ("7158300734726758", "7158 30** **** 6758"),
@@ -15,8 +14,7 @@ from src.masks import mask_number_card, mask_account_number
 def test_mask_number_card(number_card, masked_number):
     assert mask_number_card(number_card) == masked_number
 
-@pytest.mark.parametrize(
-    "account_number, masks_account",
+@pytest.mark.parametrize("account_number, masks_account",
     [
         ("73654108430135874305", "**4305"),
         ("64686473678894779589", "**9589"),
